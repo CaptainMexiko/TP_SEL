afca62fafe0bc456f42c9f7b4b3ea9eb2ce3699c
Gwendal DIDOT <didotgwendal@gmail.com>
2017-09-24 18:50:02 +0200

    Mise en place du projet

---
+++ TP_trap/trap_fonction.c
@@ -0,0 +1,28 @@
+#include "trap_fonction.h"
+
+int attach (int pid){
+  long erreurAttach1 = ptrace(PTRACE_ATTACH, pid, 0, 0);
+  if (erreurAttach1 != 0){
+    perror("PTRACE_ATTACH a l'erreur suivante :");
+    return -1;
+  }
+  return 0;
+}
+
+
+int main(int argc, char const *argv[]) {
+  if (argc == 0){
+    printf("Il manque le programme a surveiller en argument%s\n", "");
+  }
+char *str= "";
+int testAppel = snprintf(str, 500,"pgrep %s",argv[0]);
+if (testAppel != 0){
+  perror("Erreur de la chaine str : ");
+}
+int pid = system(str);
+if (pid < 1){
+  perror("erreur pid : ");
+}
+attach(pid);
+  return 0;
+}
---
+++ TP_trap/trap_fonction.h
@@ -0,0 +1,4 @@
+#include <sys/ptrace.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
