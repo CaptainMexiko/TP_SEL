2b78f54d0f7750b86b50f5cdab093b1392ee00a4
Gwendal DIDOT <didotgwendal@gmail.com>
2017-09-25 21:53:03 +0200

    Test lol pour les branches

--- TP_trap/trap_fonction.c
+++ TP_trap/trap_fonction.c
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[]) {
   if (argc == 0){
     printf("Il manque le programme a surveiller en argument%s\n", "");
   }
-char *str= argv[1];
+char const *str= argv[1];
 char cmd[100];
 int testAppel = snprintf(cmd, sizeof("pegrep") - 1 + sizeof(str),"pgrep %s", str);
 if (testAppel != 0){
