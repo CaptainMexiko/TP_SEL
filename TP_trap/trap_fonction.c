233a64c298d9fbe8ddedb19fddcace11c3eb01f6
Gwendal DIDOT <didotgwendal@gmail.com>
2017-10-04 13:29:42 +0200

    Nettoyage de main en créent la fonction getPid(char const * str)

--- TP_trap/trap_fonction.c
+++ TP_trap/trap_fonction.c
@@ -10,42 +10,46 @@ int attach (int pid){
   return 0;
 }
 
+int getPid(char const * str){
+  char cmd[MAX_LEN];
+  int testAppel = snprintf(cmd, sizeof("pgrep  > proc.txt") - 1 + sizeof(str),"pgrep %s > proc.txt", str);
+  if (testAppel < 0){
+    perror("Erreur de la chaine str ");
+    return -1;
+  }
+
+  int errPgrep= system(cmd);
+  errPgrep = WEXITSTATUS(errPgrep);
+  if (errPgrep == 1){
+    perror("Le processus n'existe pas ");
+    return -1;
+  }
+
+  int pid;
+
+  FILE * f = fopen("proc.txt", "r");
+  char numProc[MAX_LEN];
+  int sizePid = fread(numProc, 5, sizeof(char), f);
+  if (sizePid == 0){
+    perror("Erreur de fread ");
+    return -1;
+  }
+  fclose(f);
+  pid = atoi(numProc);
+
+  if (pid == 0){
+    perror("erreur pid ");
+    return -1;
+  }
+  return pid;
+}
+
 
 int main(int argc, char const *argv[]) {
   if (argc == 0){
     printf("Il manque le programme a surveiller en argument%s\n", "");
   }
-char const *str= argv[1];
-char cmd[MAX_LEN];
-int testAppel = snprintf(cmd, sizeof("pgrep  > proc.txt") - 1 + sizeof(str),"pgrep %s > proc.txt", str);
-if (testAppel < 0){
-  perror("Erreur de la chaine str ");
-  return -1;
-}
-
-int errPgrep= system(cmd);
-errPgrep = WEXITSTATUS(errPgrep);
-if (errPgrep == 1){
-  perror("Le processus n'existe pas ");
-  return -1;
-}
-
-int pid;
-
-FILE * f = fopen("proc.txt", "r");
-char numProc[MAX_LEN];
-int sizePid = fread(numProc, 5, sizeof(char), f);
-if (sizePid == 0){
-  perror("Erreur de fread ");
-  return -1;
-}
-fclose(f);
-pid = atoi(numProc);
-
-if (pid == 0){
-  perror("erreur pid ");
-  return -1;
-}
+int pid = getPid(argv[1]);
 attach(pid);
   return 0;
 }
