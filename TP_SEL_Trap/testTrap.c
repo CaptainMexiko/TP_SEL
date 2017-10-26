169cf1992b637d137991d4be3e30a86e07b67800
Alexis LeMasle <14008349@e008m05.istic.univ-rennes1.fr>
2017-10-26 17:29:18 +0200

    Modification prg de test

--- TP_SEL_Trap/testTrap.c
+++ TP_SEL_Trap/testTrap.c
@@ -2,55 +2,46 @@
 #include <stdio.h>
 
 int c1() {
-	sleep(5);
 	printf("%d\n", 1);
 	return 0;
 }
 
 int c2() {
-	sleep(5);
 	printf("%d\n", 2);
 	return 0;
 }
 
 int c3() {
-	sleep(5);
 	printf("%d\n", 3);
 	return 0;
 }
 
 int c4() {
-	sleep(5);
 	printf("%d\n", 4);
 	return 0;
 }
 
 int c5() {
-	sleep(5);
 	printf("%d\n", 5);
 	return 0;
 }
 
 int c6() {
-	sleep(5);
 	printf("%d\n", 6);
 	return 0;
 }
 
 int c7() {
-	sleep(5);
 	printf("%d\n", 7);
 	return 0;
 }
 
 int c8() {
-	sleep(5);
 	printf("%d\n", 8);
 	return 0;
 }
 
 int c9() {
-	sleep(5);
 	printf("%d\n", 9);
 	return 0;
 }
@@ -58,14 +49,23 @@ int c9() {
 int main(int argc, char const *argv[]) {
 	while (1) {
 		c1();
+		sleep(5);
 		c2();
+		sleep(5);
 		c3();
+		sleep(5);
 		c4();
+		sleep(5);
 		c5();
+		sleep(5);
 		c6();
+		sleep(5);
 		c7();
+		sleep(5);
 		c8();
+		sleep(5);
 		c9();
+		sleep(5);
 	}
 	return 0;
 }
